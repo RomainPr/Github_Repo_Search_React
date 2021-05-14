e90db24cabff3771306698cfd9f8ce1f17e24690
Rom <romainprevostdev@gmail.com>
2021-05-14 17:32:38 +0200

    Styling 404

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Segment } from 'semantic-ui-react';
 
 import './styles.scss';
 
-const Error = () => (<div className="not-found">404</div>);
+const Error = () => (
+
+  <Segment>
+    <div className="not-found">404</div>
+  </Segment>
+
+);
 
 export default Error;
